9b3736ee1b503b384be0549a773061065e46375c
ssergy <sergysh@gmail.com>
2018-12-24 22:45:19 +0300

    shell scripts checks

--- base_check_update.sh
+++ base_check_update.sh
@@ -1,4 +1,4 @@
 curl \
     -H "Content-Type: application/json" \
-    -X POST -d '{"key": "prod-aKMGjflyFUAaRkYTTBeRzykGbvYaArzE", "user_hash": "8a64174a-0ec5-49f4-9a29-47d848dc5703", "check_id": 3, "first_name": "John", "last_name": "Smith", "middle_name": "", "gender": 0, "birthday_day": "01", "birthday_month": "07", "birthday_year": "1983", "country_nationality": "SG", "country_residence": "SG", "city": "Singapore", "address": "Last Street, "address2": "Bld 1", "zip": "54321"}' \
-    http://127.0.0.1:8911/auth/base-check
+    -X POST -d '{"key": "prod-...BASISID KEY...", "user_hash": "304d3fd7-6ce1-40e7-9dc5-63a8cde68bc5", "check_id": 35350, "email": "demo@demo.com", "phone": "", "first_name": "John", "last_name": "Smith", "middle_name": "", "gender": 0, "birthday_day": "01", "birthday_month": "07", "birthday_year": "1983", "country_nationality": "SG", "country_residence": "SG", "city": "Singapore", "address": "Last Street", "address2": "Bld 1", "zip": "54321"}' \
+    https://api.basisid.com/auth/base-check
